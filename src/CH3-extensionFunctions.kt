d6aa80acbf0d2d600ba904522123fc79f661cb10
ted <tedhagos@gmail.com>
2018-02-15 20:58:14 +0800

    extension function and varargs

--- src/CH3-extensionFunctions.kt
+++ src/CH3-extensionFunctions.kt
@@ -1,25 +1,29 @@
 
 
-
-
 fun main(args: Array<String>) {
   val msg = "My name is Maximus Decimus Meridius"
-  println(homerify(msg))
-  println(chanthofy(msg))
-  println(terminatorify(msg))
+//  println(homerify(msg))
+//  println(chanthofy(msg))
+//  println(terminatorify(msg))
 
-  val util = StringUtil()
-  println(util.homerify(msg))
-  println(util.chanthofy(msg))
-  println(util.terminatorify(msg))
+//  val util = StringUtil()
+//  println(util.homerify(msg))
+//  println(util.chanthofy(msg))
+//  println(util.terminatorify(msg))
+
+  println(msg.homerify())
+
 }
 
-fun homerify(msg: String) = "$msg -- woohoo!"
-fun chanthofy(msg:String) = "Chan, $msg , tho"
-fun terminatorify(msg: String) = "$msg -- I'll be back"
+fun String.homerify() = "$this -- woohoo!"
 
-class StringUtil {
-  fun homerify(msg: String) = "$msg -- woohoo!"
-  fun chanthofy(msg:String) = "Chan, $msg , tho"
-  fun terminatorify(msg: String) = "$msg -- I'll be back"
-}
+//class StringUtil {
+//  fun homerify(msg: String) = "$msg -- woohoo!"
+//  fun chanthofy(msg:String) = "Chan, $msg , tho"
+//  fun terminatorify(msg: String) = "$msg -- I'll be back"
+//}
+//
+//fun homerify(msg: String) = "$msg -- woohoo!"
+//fun chanthofy(msg:String) = "Chan, $msg , tho"
+//fun terminatorify(msg: String) = "$msg -- I'll be back"
+
