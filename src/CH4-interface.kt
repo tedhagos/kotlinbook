d149c0d8241278775f1faf0ffaea814eecc30eb1
ted <tedhagos@gmail.com>
2018-02-20 17:21:15 +0800

    chapter 4 - interfaces

---
+++ src/CH4-interface.kt
@@ -0,0 +1,39 @@
+package tlb
+
+fun main(args: Array<String>) {
+  val mfc = MultiFunction()
+  mfc.print("The quick brown fox")
+  mfc.call("12345")
+}
+
+interface Printable {
+  fun print(doc:String) = println("Printer:Printing $doc")
+}
+
+interface Fax {
+  fun call(number: String) = println("Calling $number")
+  fun print(doc: String) = println("Fax:Printing $doc")
+  fun answer()
+}
+
+class MultiFunction : Printable, Fax {
+
+  override fun print(doc:String)  {
+    super<Fax>.print(doc)
+    super<Printable>.print(doc)
+  }
+
+   override fun answer() {
+
+  }
+}
+
+abstract class Foo {
+  abstract fun animate()
+}
+
+class Boo : Foo() {
+   override fun animate() {
+
+  }
+}
